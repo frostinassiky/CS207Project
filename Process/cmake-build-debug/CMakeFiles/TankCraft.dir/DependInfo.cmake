950d6f8451ac39b9b8c3e9bbb0e78ed6736dcd4b
Frostinassiky <xu.frost.gmail.com>
2017-11-15 21:26:15 +0300

    Debugged four GUI classes: component, container, label, button

---
+++ Process/cmake-build-debug/CMakeFiles/TankCraft.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/CommandQ.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/CommandQ.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/Entity.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/Entity.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/Game.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/Game.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/Player.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/Player.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/SceneNode.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/SceneNode.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/SpriteNode.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/SpriteNode.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/Tank.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/Tank.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/World.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/World.cpp.o"
+  "/Users/xum/Documents/CS207/Project/CS207Project/Process/main.cpp" "/Users/xum/Documents/CS207/Project/CS207Project/Process/cmake-build-debug/CMakeFiles/TankCraft.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
