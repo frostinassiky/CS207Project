4b3d82925b19e6079e74e1e0280631cf34a0fc5c
yang <yang.liu.3@kaust.edu.sa>
2017-11-18 21:56:46 +0300

    Utility.cpp Utility.h

---
+++ Process/Utility.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Yang on 18/11/2017.
+//
+
+#include "Utility.h"
+
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+
+#include <cmath>
+
+
+void centerOrigin(sf::Sprite& sprite)
+{
+    sf::FloatRect bounds = sprite.getLocalBounds();
+    sprite.setOrigin(std::floor(bounds.left + bounds.width / 2.f), std::floor(bounds.top + bounds.height / 2.f));
+}
+
+void centerOrigin(sf::Text& text)
+{
+    sf::FloatRect bounds = text.getLocalBounds();
+    text.setOrigin(std::floor(bounds.left + bounds.width / 2.f), std::floor(bounds.top + bounds.height / 2.f));
+}
